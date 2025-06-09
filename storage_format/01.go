f453c5646ae86707c2eaaf8daa1021813194a052
9bany <bany.y0599@gmail.com>
2025-06-09 13:11:57 +0700

    find row with fixed size in content

--- storage_format/01.go
+++ storage_format/01.go
@@ -26,9 +26,29 @@ func findRow(content string, r byte) string {
 	return ""
 }
 
+func findRowFixedSize(content string, r byte) string {
+	log.Println("Find row in content fixed size format")
+	for i := 0; i < len(content); i++ {
+		if content[i] == '\n' {
+			continue
+		}
+		if content[i] == r {
+			s := content[i : i+17]
+			return s
+		}
+	}
+
+	return ""
+}
+
 func main() {
 	// write the code to find row #2?
 	content := "1;my_pod\n2;pod2\n3;another_pod\n"
 	row := findRow(content, '3')
+	log.Println(row)
+
+	// fixed size
+	content = "1my_pod..........2pod2............3another_pod....."
+	row = findRowFixedSize(content, '3')
 	log.Println(row)
 }
